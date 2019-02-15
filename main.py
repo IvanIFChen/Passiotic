ee2ec10515d3f40621af62e19b7ecd2132390168
Ivan Chen <ivan.if.chen@gmail.com>
2019-02-15 15:27:56 -0500

    added debug code for teammates

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pyshark
 import logging
 
+INTERFACE = 'wlan0'
+
 def setup_log():
     logging.basicConfig(filename='main.log', filemode="a+", level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
@@ -14,23 +16,35 @@ def setup_log():
 if __name__ == '__main__':
     logger = setup_log()
 
+    # DEBUG: uncomment this line to fetch from a pre-recorded file
     # cap = pyshark.FileCapture('pcaps/tuna8-join-ddwrt.pcap', display_filter='wlan.sa[4:]==8f:28 and wlan.fc.type==0')
-    cap = pyshark.LiveCapture(interface='wlan0', monitor_mode=True, display_filter='wlan.fc.type==0')
+
+    # DEBUG: uncomment this line to debug if the interface is able to capture packets
+    cap = pyshark.LiveCapture(interface=INTERFACE)
+
+    # DEBUG: uncomment this line to debug if monitor mode works
+    # cap = pyshark.LiveCapture(interface=INTERFACE, monitor_mode=True)
+
+    # END GOAL: uncomment this line to look for only management signals
+    # cap = pyshark.LiveCapture(interface=INTERFACE, monitor_mode=True, display_filter='wlan.fc.type==0')
 
     print('Listening...')
 
     # for p in cap:
     for p in cap.sniff_continuously():
-        is_asso_req = int(p.wlan.fc_type_subtype) == 0
-        # safety check, should always be true since we have display filter on
-        if is_asso_req:
-            try:
-                ssid = p.layers[3].ssid
-                src_addr = p.wlan.sa
-                dest_addr = p.wlan.da
-                msg = 'Device \"{}\" is trying to join \"{}\"'.format(src_addr, ssid)
-                print(msg)
-                logger.debug(msg)
-            except AttributeError as e:
-                print(e)
-                continue
+
+        # DEBUG: uncomment to see all packets
+        # print(p)
+
+        try:
+            is_asso_req = int(p.wlan.fc_type_subtype) == 0
+            if is_asso_req:
+                    ssid = p.layers[3].ssid
+                    src_addr = p.wlan.sa
+                    dest_addr = p.wlan.da
+                    msg = 'Device \"{}\" is trying to join \"{}\"'.format(src_addr, ssid)
+                    print(msg)
+                    logger.debug(msg)
+        except (AttributeError, TypeError) as e:
+            print(e)
+            continue
