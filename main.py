452cbfc512cfeefdd532dfd1d494b714b1b54a96
Ivan Chen <ivan.if.chen@gmail.com>
2019-02-15 14:11:45 -0500

    added logging

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import pyshark
+import logging
 
-# cap = pyshark.FileCapture('pcaps/tuna8-join-ddwrt.pcap', display_filter='wlan.sa[4:]==8f:28 and wlan.fc.type==0')
-cap = pyshark.LiveCapture(interface='en0', monitor_mode=True, display_filter='wlan.fc.type==0')
+def setup_log():
+    logging.basicConfig(filename='main.log', filemode="a+", level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
-# for p in cap:
-for p in cap.sniff_continuously():
-    is_asso_req = int(p.wlan.fc_type_subtype) == 0
-    # safety check, should always be true since we have display filter on
-    if is_asso_req:
-        ssid = p.layers[3].ssid
-        src_addr = p.wlan.sa
-        dest_addr = p.wlan.da
-        print('Device \"{}\" is trying to join \"{}\"'.format(src_addr, ssid))
+    console = logging.StreamHandler()  
+    console.setLevel(logging.ERROR)  
+    logging.getLogger("").addHandler(console)
+
+    logger = logging.getLogger(__name__)
+    return logger
+
+if __name__ == '__main__':
+    logger = setup_log()
+
+    # cap = pyshark.FileCapture('pcaps/tuna8-join-ddwrt.pcap', display_filter='wlan.sa[4:]==8f:28 and wlan.fc.type==0')
+    cap = pyshark.LiveCapture(interface='en0', monitor_mode=True, display_filter='wlan.fc.type==0')
+
+    # for p in cap:
+    for p in cap.sniff_continuously():
+        is_asso_req = int(p.wlan.fc_type_subtype) == 0
+        # safety check, should always be true since we have display filter on
+        if is_asso_req:
+            try:
+                ssid = p.layers[3].ssid
+                src_addr = p.wlan.sa
+                dest_addr = p.wlan.da
+                msg = 'Device \"{}\" is trying to join \"{}\"'.format(src_addr, ssid)
+                print(msg)
+                logger.debug(msg)
+            except AttributeError as e:
+                print(e)
+                continue
