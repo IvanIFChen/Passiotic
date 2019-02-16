8d55c72924473b545742c5fefc999e8e829e8a6c
Ivan Chen <ivan.if.chen@gmail.com>
2019-02-16 17:05:37 -0500

    POC unique active device is ready

---
+++ unique_active_devices.py
@@ -0,0 +1,60 @@
+import pyshark
+import logging
+import time
+from pprint import pprint
+
+INTERFACE = 'en0'
+TICK = 1 # in seconds
+
+def setup_log():
+    logging.basicConfig(filename='unique_active_devices.log', filemode="a+", level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+
+    console = logging.StreamHandler()  
+    console.setLevel(logging.ERROR)  
+    logging.getLogger("").addHandler(console)
+
+    logger = logging.getLogger(__name__)
+    return logger
+
+if __name__ == '__main__':
+    logger = setup_log()
+
+    cap = pyshark.LiveCapture(interface=INTERFACE, monitor_mode=True)
+
+    print('Listening...')
+
+    try:
+        active_devices = set([])
+        before = time.time()
+
+        # for p in cap:
+        for p in cap.sniff_continuously():
+            
+            try:
+                # this is a DATA TYPE frame
+                accept = int(p.wlan.fc_type) == 2
+
+                if accept and p.highest_layer != '_WS.MALFORMED':
+                    # Note: different DATA type frames have a different structure,
+                    # based on observation, it could have 'sa' or 'addr'. If one them
+                    # does not exist we'll append a 'None', but we'll filter that away later.
+                    active_devices.add(p.wlan.get('sa'))
+                    active_devices.add(p.wlan.get('addr'))
+
+                    if time.time() - before >= TICK:
+                        # clean up addresses
+                        ignore_invalid = lambda x: x is not None and x != 'ff:ff:ff:ff:ff:ff'
+                        active_devices = {x for x in active_devices if ignore_invalid(x)}
+
+                        print('Active active_devices: {}'.format(active_devices))
+                        before = time.time()
+                        active_devices = set([])
+
+            except (AttributeError, TypeError) as e:
+                print('ERROR: {}'.format(e))
+                logger.debug(e)
+                logger.debug(p)
+                continue
+    except KeyboardInterrupt:
+        print('BYE')
+        pass
