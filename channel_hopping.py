cdb7bb1667af65eee584ef32965867baf47bcbcb
Joshua Driesman <jdriesman@outlook.com>
2019-04-09 16:39:46 -0400

    Refactored channel hoping out into separate thread.

---
+++ channel_hopping.py
@@ -0,0 +1,38 @@
+import threading
+import subprocess
+
+
+class ChannelHopper(threading.Thread):
+    """
+    A thread worker that hops between an array of channels in order every n seconds.
+    """
+
+    def __init__(self, channels, frequency, interface):
+        """
+        channels -- An array of integers representing the channels to hop between.
+        frequency -- The number of seconds between each hop.
+        interface -- The wireless interface to switch.
+        """
+        super().__init__()
+        self.channels = channels
+        self.frequency = frequency
+        self.stop = threading.Event()
+        self.interface = interface
+
+    def run(self):
+        chan_index = 0
+        while not self.stop.isSet():
+            chan_integer = self.channels[chan_index]
+            chan_string = str(
+                chan_integer) if chan_integer > 9 else '0' + str(chan_integer)
+
+            subprocess.run(
+                ['iwconfig', self.interface, 'channel', chan_string])
+
+            print('Current channel is {}'.format(chan_string))
+
+            chan_index = (chan_index + 1) % len(self.channels)
+
+    def join(self, timeout=None):
+        self.stop.set()
+        super().join(timeout)
