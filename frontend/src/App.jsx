3b7a3786cb492a01c08b4cd0bbc02ec1096ddf0d
Joshua Driesman <jdriesman@outlook.com>
2019-04-23 20:47:26 -0400

    fixed unique counter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 import Counter from './components/counter';
 import ChartComponent from './components/chart';
 
+const PI_IDS = ['pi_1', 'pi_2'];
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -28,20 +30,42 @@ class App extends Component {
       const data = await res.json();
       const items = data.Items;
 
-      let latestDate;
-      items.forEach(item => {
-        const endDateTime = Date.parse(item.end_time);
-        if (endDateTime > latestDate || latestDate === undefined) {
-          latestDate = endDateTime;
-        }
+      const piItems = new Map();
+      PI_IDS.forEach(id => {
+        const pi = items.filter(item => item.pi_id === id);
+        piItems.set(id, pi);
       });
 
-      const activeDeviceCount = items.filter(
-        item => Date.parse(item.end_time) === latestDate
-      ).length;
+      const piLatestRound = new Map();
+      for (let pi of piItems.keys()) {
+        piLatestRound.set(pi, 0);
+      }
+
+      for (let items of piItems) {
+        for (let item of items[1]) {
+          if (piLatestRound.get(items[0]) < item.round_id) {
+            piLatestRound.set(items[0], item.round_id);
+          }
+        }
+      }
+
+      const piFiltered = new Map();
+      for (let pi of piItems.keys()) {
+        const filteredItems = piItems
+          .get(pi)
+          .filter(item => item.round_id === piLatestRound.get(pi));
+        piFiltered.set(pi, filteredItems);
+      }
+
+      const uniqueDevices = new Set();
+      for (let items of piFiltered.values()) {
+        for (let item of items) {
+          uniqueDevices.add(item.device_mac);
+        }
+      }
 
       this.setState({
-        activeDeviceCount: activeDeviceCount,
+        activeDeviceCount: uniqueDevices.size,
         deviceList: items
       });
     };
